64b59a99add838ea4a911eb69a0997a507d54f91
Manon Philippot <33070721+maphdev@users.noreply.github.com>
2018-07-30 23:19:54 +0200

    Sign Up & Sign In layout.

--- app/src/main/java/com/capstone/maphdev/triviapp/MainActivity.java
+++ app/src/main/java/com/capstone/maphdev/triviapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.capstone.maphdev.triviapp;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
